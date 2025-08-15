53374b0331b05fa3f4f853a4abac9ea8cb65bd9d
ArchPsion <perso@benoitsordet.com>
2025-08-15 17:42:46 -0600

    Cleanup

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,8 @@
+// Custom Libraries
 #include "LinearAlgebra.hpp"
-#include <vector>
 
-#include <csignal>
-#include <execinfo.h>
-#include <unistd.h>
-
-void Handler(i32)
+int main(void)
 {
-	void* tab[20u];
-	
-	const auto size = backtrace(tab, 20u);
-	backtrace_symbols_fd(tab, size, STDERR_FILENO);
-	std::exit(1);
-}
-
-i32 main(void)
-{
-	struct sigaction act;
-	sigemptyset(&act.sa_mask);
-	act.sa_handler = ::Handler;
-	act.sa_flags = 0;
-	sigaction(SIGSEGV, &act, 0);
-	
 	const auto m1 = HexMatrix<f32, 2u, 2u>(1.f, 1.f, 0.f, 1.f);
 	const auto m2 = HexMatrix<f32, 2u, 3u>(2.f, 3.f, 4.f, 5.f, 6.f, 7.f);
 	const auto m3 = HexMatrix<f32, 3u, 3u>(1.f, 0.f, 0.f, 2.f, 1.f, 1.f, 0.f, 6.f, 0.f);
